c8d2a121b0feb017e395cc5e0023866941521baa
Никита Шипилов <116575516+LAYT73@users.noreply.github.com>
2026-04-17 17:10:36 +0400

    fix(V-1): add getImagePath for slider images

--- src/shared/consts/sliderSlides.ts
+++ src/shared/consts/sliderSlides.ts
@@ -1,3 +1,5 @@
+import { getImagePath } from '../utils'
+
 export interface SliderSlide {
   id: number
   title: string
@@ -12,28 +14,28 @@ export const SLIDER_SLIDES: SliderSlide[] = [
     title: 'Бесплатная парковка',
     description:
       'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
-    image: '/images/slider/slider-1.png',
+    image: getImagePath('images/slider/slider-1.png'),
     buttonVariant: 'green',
   },
   {
     id: 2,
     title: 'Страховка',
     description: 'Полная страховка автомобиля',
-    image: '/images/slider/slider-2.png',
+    image: getImagePath('images/slider/slider-2.png'),
     buttonVariant: 'cian',
   },
   {
     id: 3,
     title: 'Бензин',
     description: 'Полный бак на любой заправке города за наш счёт',
-    image: '/images/slider/slider-3.png',
+    image: getImagePath('images/slider/slider-3.png'),
     buttonVariant: 'orange',
   },
   {
     id: 4,
     title: 'Обслуживание',
-    description: 'Автомобиль проходит еженедельное ТО',
-    image: '/images/slider/slider-4.png',
+    description: 'Автомобиль проходит еженедельное ТO',
+    image: getImagePath('images/slider/slider-4.png'),
     buttonVariant: 'purple',
   },
 ]
